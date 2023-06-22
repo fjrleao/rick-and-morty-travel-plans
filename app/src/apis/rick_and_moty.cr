a567060b042a0d918067ed1c4262f6d495096c38
Fábio Júnior <fjrleao@gmail.com>
2023-06-22 16:54:39 -0300

    feat: sort result to optimize travel stops

--- app/src/apis/rick_and_moty.cr
+++ app/src/apis/rick_and_moty.cr
@@ -7,6 +7,13 @@ class RickAndMortyApiSerializable
   property locationsByIds : Array(Hash(String, String | Int32))
 end
 
+class RickAndMortyExpandedApiSerializable
+  include JSON::Serializable
+
+  property locationsByIds : Array(Hash(String, String | Array(Hash(String, String | Array(Hash(String, String))))))
+  # property locationsByIds : Array(Hash(String, String | Array(Hash(String, String))))
+end
+
 class RickAndMortyApi
   def initialize
     private @api = Api::RickAndMorty.new
@@ -21,10 +28,9 @@ class RickAndMortyApi
           type,
           dimension ,
           residents {
-            name
+            name,
             episode {
               id
-              name
             }
           }
         }
@@ -56,7 +62,62 @@ class RickAndMortyApi
   end
 
   def optimizeTravel(filter : Array(Int32))
-    data = self.locationsById(filter)
-    puts data
+    data = self.locationsById(filter, true)
+
+    calculated_data = Array(Hash(String, String | Int32)).new
+    dimension_data = Hash(String, Hash(String, Int32)).new
+    result_array = Array(Int32).new
+
+    serialized_data = RickAndMortyExpandedApiSerializable.from_json(data.to_json.to_s)
+    serialized_data.locationsByIds.each do |location|
+      residents_size = location["residents"].size
+      idx = 0
+      qtd_residents_appeared = 0
+      while idx < residents_size
+        residents = Hash(String, String | Array(Hash(String, String))).from_json(location["residents"][idx].to_json.to_s)
+        qtd_residents_appeared += residents["episode"].size
+        idx += 1
+      end
+
+      calculated_data.push({
+        "location_id"         => location["id"].to_s.to_i,
+        "location_name"       => location["name"].to_s,
+        "location_popularity" => qtd_residents_appeared,
+        "dimension_name"      => location["dimension"].to_s,
+      })
+    end
+
+    calculated_data.each do |calculated|
+      if dimension_data.has_key?(calculated["dimension_name"].to_s)
+        dimension_data[calculated["dimension_name"].to_s] = {
+          "appeared"         => dimension_data[calculated["dimension_name"].to_s]["appeared"] += 1,
+          "total_popularity" => dimension_data[calculated["dimension_name"].to_s]["total_popularity"] += calculated["location_popularity"].to_i,
+        }
+      else
+        dimension_data[calculated["dimension_name"].to_s] = {
+          "appeared"         => 1,
+          "total_popularity" => calculated["location_popularity"].to_i,
+        }
+      end
+    end
+
+    calculated_data.each do |calculated|
+      calculated["dimension_popularity"] = (dimension_data[calculated["dimension_name"].to_s]["total_popularity"] / dimension_data[calculated["dimension_name"].to_s]["appeared"]).to_i
+    end
+
+    sorted_data = calculated_data.sort_by { |hsh| [hsh["dimension_popularity"].to_i, hsh["location_popularity"].to_i] }
+
+    sorted_data = sorted_data.sort do |a, b|
+      if a["location_popularity"].to_i == b["location_popularity"].to_i
+        a["name"].to_s <=> b["name"].to_s
+      else
+        a["dimension_popularity"].to_i <=> b["dimension_popularity"].to_i
+      end
+    end
+
+    sorted_data.each do |data|
+      result_array.push(data["location_id"].to_i)
+    end
+    result_array
   end
 end
