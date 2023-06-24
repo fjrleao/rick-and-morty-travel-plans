c59f416e4d3d72e28f4bead0bd0191989977703b
Fábio Júnior <fjrleao@gmail.com>
2023-06-24 16:21:07 -0300

    test: testing POST /travel_plans

--- app/spec/app_spec.cr
+++ app/spec/app_spec.cr
@@ -1,1 +1,14 @@
 require "./spec_helper"
+
+describe App do
+  context "POST /travel_plans" do
+    it "Should be capable to store a travel_plan" do
+      body = {"travel_stops": [2, 7, 9, 11, 19]}
+
+      post "/travel_plans", body: body.to_json
+
+      json = JSON.parse(response.body)
+      json["travel_stops"].should eq [2, 7, 9, 11, 19]
+    end
+  end
+end
--- app/spec/spec_helper.cr
+++ app/spec/spec_helper.cr
@@ -1,4 +1,5 @@
 ENV["KEMAL_ENV"] = "test"
+ENV["BASE_URL_API_RICK_MORTY"] = "https://rickandmortyapi.com/graphql"
 
 require "spec"
 require "spec-kemal"
