e74759f9910d31679fec2e6ec092b8ed446ea8a9
Fábio Júnior <fjrleao@gmail.com>
2023-06-25 15:22:19 -0300

    feat: exception handling in routes

--- app/src/app.cr
+++ app/src/app.cr
@@ -11,9 +11,13 @@ module App
   end
 
   post "/travel_plans" do |env|
-    body = TravelPlanSerializer.from_json env.request.body.not_nil!
-    env.response.status_code = 201
-    TravelPlansControllers.create(body.travel_stops).to_json
+    begin
+      body = TravelPlanSerializer.from_json env.request.body.not_nil!
+      env.response.status_code = 201
+      TravelPlansControllers.create(body.travel_stops).to_json
+    rescue exception
+      halt env, status_code = 400, %({"message": "#{exception.message.not_nil!.split('\n')[0]}"})
+    end
   end
 
   get "/travel_plans" do |env|
@@ -22,10 +26,15 @@ module App
   end
 
   get "/travel_plans/:id" do |env|
+    # begin
     id = env.params.url["id"]
     query = env.params.query.to_h
     env.response.status_code = 200
     TravelPlansControllers.retrieve(id, query).to_json
+    # rescue exception
+    #   puts exception
+    #   halt env, status_code = 404, %({"message": "Travel plan not found"})
+    # end
   end
 
   delete "/travel_plans/:id" do |env|
@@ -35,15 +44,37 @@ module App
   end
 
   put "/travel_plans/:id" do |env|
-    id = env.params.url["id"]
-    body = TravelPlanSerializer.from_json env.request.body.not_nil!
-    TravelPlansControllers.update(id, body.travel_stops).to_json
+    begin
+      id = env.params.url["id"]
+      body = TravelPlanSerializer.from_json env.request.body.not_nil!
+      TravelPlansControllers.update(id, body.travel_stops).to_json
+    rescue exception
+      if exception.message.not_nil!.includes?("Missing JSON attribute")
+        status_code = 400
+        message = %({"message": "#{exception.message.not_nil!.split('\n')[0]}"})
+      else
+        status_code = 404
+        message = %({"message": "Travel plan not found"})
+      end
+      halt env, status_code, message
+    end
   end
 
   patch "/travel_plans/:id/append" do |env|
-    id = env.params.url["id"]
-    body = TravelPlanSerializer.from_json env.request.body.not_nil!
-    TravelPlansControllers.append(id, body.travel_stops).to_json
+    begin
+      id = env.params.url["id"]
+      body = TravelPlanSerializer.from_json env.request.body.not_nil!
+      TravelPlansControllers.append(id, body.travel_stops).to_json
+    rescue exception
+      if exception.message.not_nil!.includes?("Missing JSON attribute")
+        status_code = 400
+        message = %({"message": "#{exception.message.not_nil!.split('\n')[0]}"})
+      else
+        status_code = 404
+        message = %({"message": "Travel plan not found"})
+      end
+      halt env, status_code, message
+    end
   end
 
   get "/docs" do |env|
